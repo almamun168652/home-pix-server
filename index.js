10b755b0ec7ef77ae89db3446e0765fbfa17cf6e
Al Mamun <wdmamun75@gmail.com>
2023-11-26 00:25:20 +0600

    store user info on the data base

--- index.js
+++ index.js
@@ -31,6 +31,33 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         // await client.connect();
+
+
+        const userCollection = client.db("homePixDB").collection("users");
+
+
+        // users related api
+        app.post('/users', async (req, res) => {
+            try {
+                const user = req.body;
+                const query = {email: user.email}
+                const existingUser = await userCollection.findOne(query);
+                if(existingUser){
+                    return res.send({message: 'user already exists' , insertedId: null})
+                }
+                const result = await userCollection.insertOne(user);
+                res.send(result);
+            } catch (error) {
+                console.log(error);
+            }
+        })
+
+
+
+
+        
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
