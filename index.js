ab0c6c2aa1ea35df3a6caf71ee12c3e7278ad1c9
Al Mamun <wdmamun75@gmail.com>
2023-11-27 00:07:03 +0600

    added admin route private

--- index.js
+++ index.js
@@ -62,6 +62,18 @@ async function run() {
         }
 
 
+        // use verify admin after verify token
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            const isAdmin = user?.role === 'admin';
+            if (!isAdmin) {
+                return res.status(403).send({ message: 'forbidden access' });
+            }
+            next();
+        }
+
 
 
 
@@ -81,8 +93,9 @@ async function run() {
                 console.log(error);
             }
         })
-        // users get
-        app.get('/users', verifyToken, async (req, res) => {
+        // users get 
+        // only admin
+        app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
 
             const result = await userCollection.find().toArray();
             res.send(result);
