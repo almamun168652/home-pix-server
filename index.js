c29bcd16d3bfb907c2b11cd97e356adc11fcba85
Al Mamun <wdmamun75@gmail.com>
2023-11-28 18:04:56 +0600

    added delete function

--- index.js
+++ index.js
@@ -244,6 +244,14 @@ async function run() {
             res.send(result)
         })
 
+        // delete property
+        app.delete("/addedProperty/:id" , async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await propertyCollection.deleteOne(query);
+            res.send(result);
+        })
+
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
